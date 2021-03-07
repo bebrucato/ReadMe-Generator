bdc47e88e284be06878d831131316252195c3082
B Brucato <bebrucato@gmail.com>
2021-03-07 12:20:11 -0800

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -5,7 +5,58 @@ const utils = require('utils')
 const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+      {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+      }, 
+      {
+        type: 'input',
+        message: 'Provide a brief description of your project.',
+        name: 'description',
+      },
+      {
+        type: 'input',
+        message: 'Table of Contents.',
+        name: 'toc',
+      },
+      {
+        type: 'input',
+        message: 'Provide the installation instructions. What must be downloaded?',
+        name: 'installation',
+      },
+      {
+        type: 'input',
+        message: 'How is this app used?',
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: 'Who contributed to this project?',
+        name: 'contributors',
+      },
+      {
+        type: 'input',
+        message: 'How do I test this app?',
+        name: 'test',
+      },
+      {
+        type: 'input',
+        message: 'What license are you using?',
+        name: 'license',
+      },
+      {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'username',
+      },
+      {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+      },
+];
  
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
